fd9b42a0fa90913cd1ae51153c2da53fc9992aa1
NAP-Software <info@nap.software>
2022-03-21 13:04:13 +0100

    Include branch in version (dev and nightly).

--- _get_release_version.php
+++ _get_release_version.php
@@ -10,7 +10,7 @@ return function() {
 	$git = (require __DIR__."/_git.php")();
 
 	if (in_array($git["branch"], ["nightly", "dev"])) {
-		return substr($git["HEAD"], -8, 8);
+		return $git["branch"]."-".substr($git["HEAD"], -8, 8);
 	}
 
 	return $git["release_version"];
