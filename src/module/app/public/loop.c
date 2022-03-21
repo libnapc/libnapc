d8382763208faccebe3a7a3a39deceba06af4d81
NAP-Software <info@nap.software>
2022-03-21 13:27:03 +0100

    Fix weak attributes.

--- src/module/app/public/loop.c
+++ src/module/app/public/loop.c
@@ -2,13 +2,16 @@
 
 static bool _previous_link_status = false; // @static
 
-bool napc_app_loop(napc_uptime uptime) NAPC_FN_WEAK() {
+bool napc_app_loop(napc_uptime uptime) NAPC_FN_WEAK();
+bool napc_loop(napc_time uptime) NAPC_FN_WEAK();
+
+bool napc_app_loop(napc_uptime uptime) {
 	napc_printf("napc_app_loop() is not defined.\n");
 
 	return false;
 }
 
-bool napc_loop(napc_time uptime) NAPC_FN_WEAK() {
+bool napc_loop(napc_time uptime) {
 	bool link_status = napc_eth_getLinkStatus();
 
 	napc_random_collectBytes();
--- src/module/app/public/setup.c
+++ src/module/app/public/setup.c
@@ -1,9 +1,12 @@
 #include <module/app/_private/_app.h>
 
-void napc_app_setup(const char *platform) NAPC_FN_WEAK() {
+void napc_app_setup(const char *platform) NAPC_FN_WEAK();
+void napc_setup(const char *platform) NAPC_FN_WEAK();
+
+void napc_app_setup(const char *platform) {
 	napc_printf("napc_app_setup() is not defined.\n");
 }
 
-void napc_setup(const char *platform) NAPC_FN_WEAK() {
+void napc_setup(const char *platform) {
 	napc_app_setup(platform);
 }
