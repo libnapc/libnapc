188c6a565447ed1973fb3a349de2c135f05dd27b
NAP-Software <info@nap.software>
2022-04-09 03:53:31 +0200

    Updated documentation for IPv4Participant.

--- src/module/ipv4participant/ipv4participant.h
+++ src/module/ipv4participant/ipv4participant.h
@@ -1,3 +1,16 @@
+/*!
+ * @name IPv4Participant:intro
+ * @description
+ * To reduce parameter count this module is used to combine an IPv4-Address and a port number:
+ * 
+ * ```c
+ * // Holds address for google's DNS server 8.8.8.8:53
+ * napc__IPv4Participant google_dns;
+ * 
+ * // Initialize google_dns variable
+ * napc_IPv4Participant_init(&google_dns, "8.8.8.8", 53);
+ * ```
+ */
 #if !defined(NAPC_MODULE_IPV4PARTICIPANT_h)
 	#define NAPC_MODULE_IPV4PARTICIPANT_h
 
