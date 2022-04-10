8ba19bbef43e5f66a0fde4f328af0fe870fe22ff
NAP-Software <info@nap.software>
2022-04-10 02:42:46 +0200

    This fixes issue #65: Default to: set fs root path to cwd on linux.

--- src/hw.module/file/HAL/linux/HAL_napc_File_open.c
+++ src/hw.module/file/HAL/linux/HAL_napc_File_open.c
@@ -1,9 +1,20 @@
 #if !defined(ARDUINO)
 	#include <hw.module/file/_private/_file.h>
 	#include <stdio.h> // FILE, fopen(), fileno()
+	#include <napc-utils/napc-utils.h> // napc_snprintf()
 
 	bool HAL_napc_File_open(void *ptr, const char *path, const char *mode) {
-		FILE *fp = fopen(path, mode);
+		char realpath[512];
+
+		const char *FS_ROOT_DIR = getenv("NAPC_FS_ROOT_DIR");
+
+		if (!FS_ROOT_DIR) {
+			FS_ROOT_DIR = "";
+		}
+
+		napc_snprintf(realpath, sizeof(realpath), "%s%s", FS_ROOT_DIR, path);
+
+		FILE *fp = fopen(realpath, mode);
 
 		if (!fp) {
 			return false;
