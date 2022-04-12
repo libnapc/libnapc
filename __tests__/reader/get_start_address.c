7e43fcba3313a8124b9bd9efca19fef96be126e6
NAP-Software <info@nap.software>
2022-04-12 03:53:09 +0200

    Added tests for napc_Reader_getStartAddress.

---
+++ __tests__/reader/get_start_address.c
@@ -0,0 +1,16 @@
+#include <napc.h>
+#include <napc-testing.h>
+
+TEST_CASE("should work as expected") {
+	unsigned char buffer[5];
+	unsigned char *start = buffer;
+
+	napc__Reader reader = napc_Reader_create(buffer, sizeof(buffer));
+
+	// Read 3 bytes
+	NAPC_IGNORE_VALUE(napc_Reader_readU8Array(&reader, 3, NULL));
+
+	TEST_ASSERT(
+		napc_Reader_getStartAddress(&reader) == start
+	);
+}
