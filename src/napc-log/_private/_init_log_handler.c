1b8175849ccf6ea3e3f5f27402743998f70166be
NAP-Software <info@nap.software>
2022-04-14 13:54:33 +0200

    Renamed napc_initLogHandler to PV_napc_initLogHandler.

--- src/napc-log/_private/_init_log_handler.c
+++ src/napc-log/_private/_init_log_handler.c
@@ -1,6 +1,6 @@
 #include <napc-log/_private/_napc-log.h>
 
-void napc_initLogHandler(void) {
+void PV_napc_initLogHandler(void) {
 	for (napc_size i = 0; i < NAPC_ARRAY_ELEMENTS(PV_napc_log_handler_array); ++i) {
 		PV_napc_log_handler_array[i] = NULL;
 	}
