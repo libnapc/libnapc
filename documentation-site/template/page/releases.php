79be37ebe5e99aaaa8ba1e97becec436db41cfbc
NAP-Software <info@nap.software>
2022-08-15 00:37:02 +0200

    Made release download links compatible with stash.nap-software.com file server.

--- documentation-site/template/page/releases.php
+++ documentation-site/template/page/releases.php
@@ -22,6 +22,7 @@ div.releases div.flex-table-row-cell.filename {
 		<?php
 			$files = napcdoc::site_getDocumentation()["files"];
 
+			$git_branch = napcdoc::site_getDocumentation()["build_constants"]["GIT_BRANCH"];
 			$git_hash = napcdoc::site_getDocumentation()["build_constants"]["GIT_HEAD_HASH"];
 
 			foreach ($files as $filename => $meta) {
@@ -29,7 +30,7 @@ div.releases div.flex-table-row-cell.filename {
 
 			<div class="flex-table-row">
 				<div class="flex-table-row-cell filename">
-					<a href="https://libnapc.nap.software/releases/<?php echo "$git_hash/$filename"; ?>" target="_blank">
+					<a href="https://stash.nap-software.com/<?php echo hash("sha1", "$git_branch@$git_hash"); ?>/<?php echo $filename; ?>" target="_blank">
 						<?php echo $filename; ?>
 					</a>
 				</div>
