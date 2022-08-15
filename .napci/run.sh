9570521a3f2a7bb3a0e1e6143dae5bf5a504913e
NAP-Software <info@nap.software>
2022-08-15 03:30:45 +0200

    .napci/run.sh: cp files into .napci/upload_files.

--- .napci/run.sh
+++ .napci/run.sh
@@ -20,12 +20,11 @@ mv .napci/build_files.tmp .napci/build_files
 # upload_files
 cd .napci/upload_files.tmp/
 
-ln -s ../build_files/libnapc-arduino.zip .
-ln -s ../build_files/libnapc-linux-aarch64.a .
-ln -s ../build_files/libnapc-linux-x86_64.a .
-ln -s ../build_files/napc.h .
+cp ../build_files/libnapc-arduino.zip .
+cp ../build_files/libnapc-linux-aarch64.a .
+cp ../build_files/libnapc-linux-x86_64.a .
+cp ../build_files/napc.h .
 
 cd ../../
 
 mv .napci/upload_files.tmp .napci/upload_files
-
