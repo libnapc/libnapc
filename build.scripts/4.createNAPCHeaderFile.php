cbea762f856b79b49d0e60f783a0bbf95288ffa7
SZ-Coder <sz-coder@nap.software>
2022-08-31 23:47:40 +0200

    - Migrate createNAPCHeaderFile build step to napphp. - Don't add "LICENSE" to napc.h because the license is already included in the original file.

--- build.scripts/4.createNAPCHeaderFile.php
+++ build.scripts/4.createNAPCHeaderFile.php
@@ -1,22 +1,14 @@
 <?php
 
 return function() {
-	$file_contents = "/*\n";
-
-	foreach (file("LICENSE") as $line) {
-		$file_contents .= "* $line";
-	}
-
-	$file_contents .= "*/\n";
-
-	$file_contents .= XPHPUtils::libnapc_inlineHeaderIncludes(
+	$file_contents = XPHPUtils::libnapc_inlineHeaderIncludes(
 		"build/napc.h", "build/"
 	);
 
-	if (is_file("build/__tests__/__tests__.h")) {
+	if (napphp::fs_isFile("build/__tests__/__tests__.h")) {
 		$file_contents .= "\n\n";
-		$file_contents .= file_get_contents("build/__tests__/__tests__.h");
+		$file_contents .= napphp::fs_readFileString("build/__tests__/__tests__.h");
 	}
 
-	file_put_contents("build/napc.h", $file_contents);
+	napphp::fs_writeFileStringAtomic("build/napc.h", $file_contents);
 };
