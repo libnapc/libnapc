d4238289f6fb9d9122e14a20ddcbe7a7f5d66afa
SZ-Coder <sz-coder@nap.software>
2022-09-01 00:28:50 +0200

    Migrate packageArduinoLibrary build step to napphp.

--- build.scripts/6.packageArduinoLibrary.php
+++ build.scripts/6.packageArduinoLibrary.php
@@ -1,52 +1,52 @@
 <?php
 
 return function() {
-	$entries = XPHPUtils::fs_scandirRecursive("build");
+	$entries = napphp::fs_scandirRecursive("build");
 
-	$directories = array_filter($entries, function($entry) {
-		return $entry["type"] === "dir";
+	$directories = napphp::arr_filter($entries, function($entry) {
+		return $entry["type"] === "directory";
 	});
 
 	foreach ($directories as $directory) {
-		$directory = $directory["rel_path"];
+		$directory = $directory["relative_path"];
 
-		if (is_dir("build/arduino_files/src/$directory")) continue;
-
-		mkdir("build/arduino_files/src/$directory", 0777, true);
+		napphp::fs_mkdir("build/arduino_files/src/$directory");
 	}
 
-	$source_files = array_filter($entries, function($entry) {
+	$source_files = napphp::arr_filter($entries, function($entry) {
 		if ($entry["type"] !== "file") return false;
 
-		if (substr($entry["basename"], -2, 2) === ".c") return true;
-		if (substr($entry["basename"], -2, 2) === ".h") return true;
-		if (substr($entry["basename"], -4, 4) === ".hpp") return true;
+		if (napphp::str_endsWith($entry["basename"], ".c")) return true;
+		if (napphp::str_endsWith($entry["basename"], ".h")) return true;
+		if (napphp::str_endsWith($entry["basename"], ".hpp")) return true;
 
-		return substr($entry["basename"], -4, 4) === ".cpp";
+		return napphp::str_endsWith($entry["basename"], ".cpp");
 	});
 
-	mkdir("build/arduino_files/src", 0777, true);
-
 	foreach ($source_files as $source_file) {
-		$dest_path = "build/arduino_files/src/".$source_file["rel_path"];
+		$dest_path = "build/arduino_files/src/".$source_file["relative_path"];
 
-		copy($source_file["abs_path"], $dest_path);
+		napphp::fs_copyFile($source_file["path"], $dest_path);
 	}
 
-	copy("build/library.properties", "build/arduino_files/library.properties");
-	copy("LICENSE", "build/arduino_files/LICENSE");
-	copy("README.md", "build/arduino_files/README.md");
+	napphp::fs_copyFile("build/library.properties", "build/arduino_files/library.properties");
+	napphp::fs_copyFile("LICENSE", "build/arduino_files/LICENSE");
+	napphp::fs_copyFile("README.md", "build/arduino_files/README.md");
 
-	foreach (scandir("arduino_examples") as $example) {
-		mkdir("build/arduino_files/examples/$example", 0777, true);
+	foreach (napphp::fs_scandir("arduino_examples") as $example) {
+		napphp::fs_mkdir("build/arduino_files/examples/$example");
 
-		copy(
+		napphp::fs_copyFile(
 			"arduino_examples/$example/$example.ino",
 			"build/arduino_files/examples/$example/$example.ino",
 		);
 	}
 
-	XPHPUtils::shell_assertSystemCall(
-		"cd build/arduino_files/ && zip -r ../lib/libnapc-arduino.zip ."
-	);
+	napphp::proc_changeWorkingDirectory("build/arduino_files", function() {
+		napphp::shell_execTransparently(
+			"zip -r ../lib/libnapc-arduino.zip ."
+		);
+	});
+
+	exit();
 };
