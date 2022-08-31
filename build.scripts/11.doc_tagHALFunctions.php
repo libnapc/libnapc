cd527c1c542e8589bd7ebbed2638b0583bd6aa22
SZ-Coder <sz-coder@nap.software>
2022-09-01 00:39:25 +0200

    Migrate doc_tagHALFunctions build step to napphp.

--- build.scripts/11.doc_tagHALFunctions.php
+++ build.scripts/11.doc_tagHALFunctions.php
@@ -1,11 +1,11 @@
 <?php
 
 return function() {
-	$nm_output = implode("\n", XPHPUtils::shell_assertExecCall(
+	$nm_output = napphp::arr_join(XPHPUtils::shell_assertExecCall(
 		"nm", ["build/lib/libnapc-local.a"]
-	));
+	), "\n");
 
-	$symbols = json_decode(file_get_contents("build/doc/symbols.json"), true);
+	$symbols = napphp::fs_readFileJSON("build/doc/symbols.json");
 
 	foreach ($symbols["functions"] as $fn_name => &$_) {
 		$has_hal = !!strlen(strstr($nm_output, "HAL_".$fn_name));
@@ -15,9 +15,9 @@ return function() {
 		}
 	}
 
-	file_put_contents(
+	napphp::fs_writeFileJSONAtomic(
 		"build/doc/symbols.tagged.json",
-		json_encode($symbols, JSON_PRETTY_PRINT)
+		$symbols
 	);
 };
 
