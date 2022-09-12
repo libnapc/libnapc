0335d8b0564426c5d7230cf6ef7197dfb96383c4
SZ-Coder <sz-coder@nap.software>
2022-09-12 22:17:28 +0200

    Fix PHP error in preprocess build step (because of pass variable by reference).

--- bin/libnapc/bundle/index.php
+++ bin/libnapc/bundle/index.php
@@ -19,11 +19,13 @@ return [
 			return $entry["type"] === "directory";
 		});
 
-		command_runSteps("bundle", $args, [
+		$context = [
 			"output_dir" => $temp_dir,
 			"preprocessed_files" => $preprocessed_files,
 			"preprocessed_files_tree" => $preprocessed_files_tree
-		]);
+		];
+
+		command_runSteps("bundle", $args, $context);
 
 		napphp::fs_rename(
 			$temp_dir, LIBNAPC_BUILD_FILES_DIR."/bundles/"
