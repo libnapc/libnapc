4e50d77cffc16250b19640b0dd14f0933acd5a76
SZ-Coder <sz-coder@nap.software>
2022-09-12 22:59:27 +0200

    Automatically add note that a type is opaque when definition has "@opaque" doc-block.

--- documentation-site/template/component/definition/notes.php
+++ documentation-site/template/component/definition/notes.php
@@ -1,11 +1,20 @@
 <?php
 
-if (strlen($__keys["general_info"]["notes"] ?? "")) {
+$is_opaque_type = false;
+$opaque_type_notes_text = "";
+
+if ($__keys["type"] === "type" && $__keys["kind"] === "opaque") {
+	$is_opaque_type = true;
+	$opaque_type_notes_text = "Treat this type as opaque.";
+}
+
+if (strlen($__keys["general_info"]["notes"] ?? "") || $is_opaque_type) {
 	echo '<div class="module-section">';
 	echo napcdoc::site_renderTemplateFile("component/definition/section-title", [
 		"label" => "Notes"
 	]);
 
+	echo $opaque_type_notes_text;
 	echo napcdoc::format_multiLine($__keys["general_info"]["notes"]);
 	echo '</div>';
 }
