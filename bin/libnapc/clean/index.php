715198351b4f18dc73049d6ae74bd58b1e7f296b
SZ-Coder <sz-coder@nap.software>
2022-09-12 22:41:10 +0200

    Fix typo in 'libnapc clean' description.

--- bin/libnapc/clean/index.php
+++ bin/libnapc/clean/index.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	"description" => "Discard all generated build files",
+	"description" => "Discard all generated build files.",
 
 	"run" => function() {
 		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/processed_files/");
