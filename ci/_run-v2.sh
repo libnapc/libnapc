d6ef58ffaf1f3065f0e769d43fd37d963c3436fd
SZ-Coder <sz-coder@nap.software>
2022-09-13 23:38:31 +0200

    Update ci script to create (for testing purposes) ALL build files on private napci server.

--- ci/_run-v2.sh
+++ ci/_run-v2.sh
@@ -5,4 +5,21 @@
 ./bin/libnapc.php compile
 ./bin/libnapc.php link
 ./bin/libnapc.php run-unit-tests
-#./bin/libnapc.php run-linux-tests
+
+# temporarily allow unbound variables
+set +u
+
+# Create ALL output files when running on private CI-server instance
+if [ -n "$NAPCI_GIT_BRANCH" ]; then
+	./bin/libnapc.php bundle
+	./bin/libnapc.php doc-clean
+	./bin/libnapc.php doc-gen-ast
+	./bin/libnapc.php doc-extract-c-symbols
+	./bin/libnapc.php doc-extract-doc-blocks
+	./bin/libnapc.php doc-merge
+	./bin/libnapc.php doc-map
+	./bin/libnapc.php doc-finalize
+	./bin/libnapc.php doc-export
+fi
+
+set -u
