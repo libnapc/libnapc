b92ecada5ff67aeded93abd9737babf1bfcf43f7
SZ-Coder <sz-coder@nap.software>
2022-09-15 15:52:56 +0200

    Update 'libnapc clean' to remove build_constants.json file generated by 'libnapc preprocess'.

--- bin/libnapc/clean/index.php
+++ bin/libnapc/clean/index.php
@@ -5,6 +5,7 @@ return [
 
 	"run" => function() {
 		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/processed_files/");
+		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/build_constants.json");
 		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/objects/");
 		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/libnapc.a");
 		napphp::fs_delete(LIBNAPC_BUILD_FILES_DIR."/bundles/");
