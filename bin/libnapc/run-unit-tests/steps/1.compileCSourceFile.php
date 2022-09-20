4d2c7a74a5710c7b04deead7289f3035553af1d8
SZ-Coder <sz-coder@nap.software>
2022-09-21 00:48:09 +0200

    Migrate 'libnapc run-unit-tests' to new nap-software/napphp version

--- bin/libnapc/run-unit-tests/steps/1.compileCSourceFile.php
+++ bin/libnapc/run-unit-tests/steps/1.compileCSourceFile.php
@@ -3,13 +3,18 @@
 return function($args, &$context) {
 	$output_file = napphp::tmp_createFile();
 
-	napphp::shell_execTransparently("gcc", [
-		"-Wall", "-Wpedantic", "-Werror", "-Wextra",
-		"-L/".$context["library_path"],
-		"-I/".$context["include_path"],
-		$context["c_source_file"],
-		"-lnapc",
-		"-o", $output_file
+	napphp::shell_execute("gcc", [
+		"args" => [
+			"-Wall",
+			"-Wpedantic",
+			"-Werror",
+			"-Wextra",
+			"-L/".$context["library_path"],
+			"-I/".$context["include_path"],
+			$context["c_source_file"],
+			"-lnapc",
+			"-o", $output_file
+		]
 	]);
 
 	$context["executable"] = $output_file;
--- bin/libnapc/run-unit-tests/steps/2.executeWithValgrind.php
+++ bin/libnapc/run-unit-tests/steps/2.executeWithValgrind.php
@@ -1,12 +1,14 @@
 <?php
 
 return function($args, &$context) {
-	napphp::shell_execTransparently(
+	napphp::shell_execute(
 		"valgrind", [
-			"--leak-check=full",
-			"--show-reachable=yes",
-			"--error-exitcode=1",
-			$context["executable"]
+			"args" => [
+				"--leak-check=full",
+				"--show-reachable=yes",
+				"--error-exitcode=1",
+				$context["executable"]
+			]
 		]
 	);
 };
