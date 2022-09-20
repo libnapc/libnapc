6a2729425ad2f79d7e756aea6a1cc6779eed36fe
SZ-Coder <sz-coder@nap.software>
2022-09-21 00:47:12 +0200

    Migrate 'libnapc doc-export' to new nap-software/napphp version

--- bin/libnapc/doc-export/steps/5.createTARArchive.php
+++ bin/libnapc/doc-export/steps/5.createTARArchive.php
@@ -1,15 +1,15 @@
 <?php
 
 return function($args, &$context) {
-	$output_dir = $context["output_dir"];
-
-	napphp::proc_changeWorkingDirectory(
-		$output_dir, function() use ($context) {
-			napphp::shell_execTransparently(
-				"fakeroot", [
-					"tar", "-czvf", $context["output_tar"], "."
-				]
-			);
-		}
+	napphp::shell_execute(
+		"fakeroot", [
+			"cwd" => $context["output_dir"],
+			"args" => [
+				"tar",
+				"-czvf",
+				$context["output_tar"],
+				"."
+			]
+		]
 	);
 };
