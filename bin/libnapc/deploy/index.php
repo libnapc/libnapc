ca9dcb7cdd471420a6e008f946b99589ba60a9c4
SZ-Coder <sz-coder@nap.software>
2022-09-22 01:51:06 +0200

    Add 'libnapc deploy' this will download a tar.gz from static.nap-software.com and then execute a script (index.php) from that archive.

---
+++ bin/libnapc/deploy/index.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+	"description" => [
+		"Deploy to github and official website.",
+		"",
+		"Make sure that the repository's secrets are properly set:",
+		"",
+		"  - SSH-Key used for commit signing (.secrets/keys/commit_signing/id_rsa)",
+		"  - SSH-Key used to push commits to github repositories (.secrets/keys/github_push/id_rsa)",
+		"  - SSH-Key used to upload files to web-hosting (.secrets/keys/site_deploy/id_rsa)",
+		"  - Github Access Token (.secrets/github_access_token) to set default branches (post-deploy)"
+	],
+
+	"depends_on" => [
+		"bundles",
+		"documentation.tar.gz"
+	],
+
+	"run" => function($args) {
+		$dry_run = $args["flags"]["dry-run"] ?? false;
+		$deploy_args = [];
+
+		if ($dry_run) {
+			array_push($deploy_args, "--dry-run");
+		}
+
+		$release_archive_file = napphp::tmp_createFile(".tar.gz");
+
+		napphp::shell_execute(
+			"curl", [
+				"args" => [
+					"https://static.nap-software.com/github/libnapc/software-release.tar.gz",
+					"-o",
+					$release_archive_file
+				]
+			]
+		);
+
+		$archive_files = napphp::tmp_createDirectory();
+
+		napphp::shell_execute(
+			"tar", [
+				"args" => [
+					"-xzvf",
+					$release_archive_file,
+					"-C",
+					$archive_files
+				],
+				"stdout" => "/dev/null"
+			]
+		);
+
+		$exit_code = napphp::shell_execute(
+			"$archive_files/index.php", [
+				"cwd" => LIBNAPC_PROJECT_ROOT_DIR,
+				"args" => $deploy_args,
+				"allow_non_zero_exit_code" => true
+			]
+		);
+
+		if ($exit_code !== 0) {
+			throw new CommandError("software-release script failed with exit code '$exit_code'.");
+		}
+	}
+];
