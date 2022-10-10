43c48150284bd4f56caed3cf37bead94572c7aa0
SZ-Coder <sz-coder@nap.software>
2022-10-11 00:42:51 +0200

    documentation: added alternative logo for mobile users.

--- src-documentation-interface/template/component/header/logo.php
+++ src-documentation-interface/template/component/header/logo.php
@@ -5,7 +5,16 @@
 		], napcdoc::html_createImageElement(
 			"libnapc.png", [
 				"height" => 50,
-				"class" => "display-block-on-dark-theme"
+				"class" => "dark-logo"
+			]
+		));
+
+		echo napcdoc::html_createElement("a", [
+			"href" => napcdoc::site_link("index.html")
+		], napcdoc::html_createImageElement(
+			"libnapc-c.png", [
+				"height" => 50,
+				"class" => "dark-logo-mobile"
 			]
 		));
 	?>
@@ -16,7 +25,16 @@
 		], napcdoc::html_createImageElement(
 			"libnapc-alt.png", [
 				"height" => 50,
-				"class" => "display-block-on-light-theme"
+				"class" => "light-logo"
+			]
+		));
+
+		echo napcdoc::html_createElement("a", [
+			"href" => napcdoc::site_link("index.html")
+		], napcdoc::html_createImageElement(
+			"libnapc-c-alt.png", [
+				"height" => 50,
+				"class" => "light-logo-mobile"
 			]
 		));
 	?>
