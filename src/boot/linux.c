93d1c7562fa4b7d5ab155ea56bc52e9b3aeb7986
SZ-Coder <sz-coder@nap.software>
2022-11-15 02:07:43 +0100

    Save 'argc' and 'argv' in global variables to allow program to access them later (if only meant to be run on linux, of course). It's a bit hacky, and probably will be removed some time in the future with an API.

--- src/boot/linux.c
+++ src/boot/linux.c
@@ -1,8 +1,14 @@
 #if !defined(ARDUINO)
+	int PV_napc_linux_argc = 0;// @global
+	const char **PV_napc_linux_argv = NULL; // @global
+
 	void libnapc_callBootFunctions(void);
 	void napc_main(const char *platform);
 
-	int main(void) {
+	int main(int argc, const char **argv) {
+		PV_napc_linux_argc = argc;
+		PV_napc_linux_argv = argv;
+
 		libnapc_callBootFunctions();
 		napc_main("linux");
 
