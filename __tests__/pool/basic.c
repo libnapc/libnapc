adeca8477ad9665f87e770cbb2b3e63ce54ab98e
SZ-Coder <sz-coder@nap.software>
2022-11-23 10:27:32 +0100

    Don't use 'NAPC_ASSERT' inside unit tests. (use 'TEST_ASSERT' instead).

--- __tests__/pool/basic.c
+++ __tests__/pool/basic.c
@@ -20,24 +20,24 @@ TEST_CASE("should work correctly") {
 
 	napc_Pool_init(&pool, NULL, my_array, 5, sizeof(struct napc_pool_test_struct));
 
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 0);
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 1);
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 2);
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 3);
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 4);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 0);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 1);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 2);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 3);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 4);
 
-	NAPC_ASSERT(napc_Pool_getAddress(&pool, 0) == addr_element0);
-	NAPC_ASSERT(napc_Pool_getAddress(&pool, 1) == addr_element1);
-	NAPC_ASSERT(napc_Pool_getAddress(&pool, 2) == addr_element2);
-	NAPC_ASSERT(napc_Pool_getAddress(&pool, 3) == addr_element3);
-	NAPC_ASSERT(napc_Pool_getAddress(&pool, 4) == addr_element4);
+	TEST_ASSERT(napc_Pool_getAddress(&pool, 0) == addr_element0);
+	TEST_ASSERT(napc_Pool_getAddress(&pool, 1) == addr_element1);
+	TEST_ASSERT(napc_Pool_getAddress(&pool, 2) == addr_element2);
+	TEST_ASSERT(napc_Pool_getAddress(&pool, 3) == addr_element3);
+	TEST_ASSERT(napc_Pool_getAddress(&pool, 4) == addr_element4);
 
 	// element 3 should be available after this call
 	napc_Pool_releaseElement(&pool, 3);
 
-	NAPC_ASSERT(napc_Pool_claimElement(&pool) == 3);
+	TEST_ASSERT(napc_Pool_claimElement(&pool) == 3);
 
 	unsigned char *tmp = napc_Pool_getAddress(&pool, 3);
 
-	NAPC_ASSERT(tmp == addr_element3);
+	TEST_ASSERT(tmp == addr_element3);
 }
--- __tests__/sha/basic.c
+++ __tests__/sha/basic.c
@@ -5,11 +5,11 @@ TEST_CASE("It should work properly") {
 	char out[65];
 	const char *str = "secret_string";
 
-	NAPC_ASSERT(
+	TEST_ASSERT(
 		napc_sha_calculate(str, libnapc_strlen(str), out, sizeof(out))
 	);
 
-	NAPC_ASSERT(
+	TEST_ASSERT(
 		memcmp(
 			out,
 			"850f9c6966cc780194218689986e2f8635764196f5a78b8021566a7a3bddbbcc\x00",
@@ -22,11 +22,11 @@ TEST_CASE("It should work with empty string") {
 	char out[65];
 	const char *str = "";
 
-	NAPC_ASSERT(
+	TEST_ASSERT(
 		napc_sha_calculate(str, libnapc_strlen(str), out, sizeof(out))
 	);
 
-	NAPC_ASSERT(
+	TEST_ASSERT(
 		memcmp(
 			out,
 			"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\x00",
@@ -39,7 +39,7 @@ TEST_CASE("It should fail with a too small output buffer") {
 	char out[64];
 	const char *str = "";
 
-	NAPC_ASSERT(
+	TEST_ASSERT(
 		!napc_sha_calculate(str, libnapc_strlen(str), out, sizeof(out))
 	);
 }
