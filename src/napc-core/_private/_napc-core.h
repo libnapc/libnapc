dfcfd67bdf8ba6019bf9c9e6796b8f11f3c74211
SZ-Coder <sz-coder@nap.software>
2022-11-23 10:42:40 +0100

    Update header include comment.

--- src/napc-core/_private/_napc-core.h
+++ src/napc-core/_private/_napc-core.h
@@ -9,7 +9,7 @@
 
 	#include <napc-serial/napc-serial.h> // napc_printf()
 	#include <napc-time/napc-time.h> // libnapc_getTimeSinceBoot()
-	#include <napc-delay/napc-delay.h> // napc_delay()
+	#include <napc-delay/napc-delay.h> // libnapc_delay()
 	#include <napc-utils/napc-utils.h> // libnapc_streqli()
 
 	extern libnapc__AccessFailureMode PV_napc_default_access_failure_mode; // @global
