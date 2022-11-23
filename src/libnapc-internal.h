b46aa92c9a1dc57e090936e35cd216e79d3df926
SZ-Coder <sz-coder@nap.software>
2022-11-23 11:29:17 +0100

    Move macro definitions to top of file.

--- src/libnapc-internal.h
+++ src/libnapc-internal.h
@@ -1,15 +1,14 @@
 #if !defined(LIBNAPC_INTERNAL_h)
 	#define LIBNAPC_INTERNAL_h
 
+	#define LIBNAPC_CONCAT_(a, b) a ## b
+	#define LIBNAPC_CONCAT(a, b) LIBNAPC_CONCAT_(a, b)
+
 	#include <stdbool.h> // bool
 	#include <stdlib.h> // free()
 	#include <stddef.h> // NULL
 	#include <stdarg.h> // va_list
 
 	#include <libnapc-compiler.h>
-
-	#define LIBNAPC_CONCAT_(a, b) a ## b
-	#define LIBNAPC_CONCAT(a, b) LIBNAPC_CONCAT_(a, b)
-
 	#include <inttypes/inttypes.h>
 #endif
