eafd1f3f038afa367414b924fb7eda2ab099d0fc
SZ-Coder <sz-coder@nap.software>
2022-11-23 11:26:05 +0100

    Rename 'NAPC_CONCAT' to 'LIBNAPC_CONCAT'.

--- src/libnapc-internal.h
+++ src/libnapc-internal.h
@@ -7,8 +7,8 @@
 
 	#include <libnapc-compiler.h>
 
-	#define NAPC_CONCAT_(a, b) a ## b
-	#define NAPC_CONCAT(a, b) NAPC_CONCAT_(a, b)
+	#define LIBNAPC_CONCAT_(a, b) a ## b
+	#define LIBNAPC_CONCAT(a, b) LIBNAPC_CONCAT_(a, b)
 
 	#include <inttypes/inttypes.h>
 #endif
