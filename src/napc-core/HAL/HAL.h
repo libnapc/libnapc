b1c670b14b08d9c939159a53c3b164490514a69a
SZ-Coder <sz-coder@nap.software>
2022-11-23 06:33:56 +0100

    Clean up 'napc-core' module: - remove references to 'HAL_napc_loopYieldCPU' and friends.

--- src/napc-core/HAL/HAL.h
+++ src/napc-core/HAL/HAL.h
@@ -3,7 +3,5 @@
 
 	#include <napc-core/napc-core.h>
 
-	void HAL_napc_loopYieldCPU(bool dry_run);
-
 	napc_size HAL_napc_getFreeMemory(void) NAPC_FN_WARNUNUSED_RET();
 #endif
--- src/napc-core/HAL/linux/HAL_napc_determineLoopYieldCPUDelay.c
+++
@@ -1,7 +0,0 @@
-#if !defined(ARDUINO)
-	#include <napc-core/_private/_napc-core.h>
-
-	void HAL_napc_determineLoopYieldCPUDelay(void) {
-		HAL_napc_loopYieldCPU(true);
-	}
-#endif
