22bc5c535497e136da5ae81ef0d1a6bc54ad16f2
SZ-Coder <sz-coder@nap.software>
2022-11-23 11:26:31 +0100

    Remove empty line.

--- src/version.c
+++ src/version.c
@@ -2,7 +2,6 @@ const char *libnapc_version(void) {
 	return "%BC_RELEASE_VERSION%";
 }
 
-
 const char *libnapc_getFullVersion(void) {
 	return "%BC_RELEASE_VERSION% (git commit '%BC_GIT_HEAD_HASH%')";
 }
