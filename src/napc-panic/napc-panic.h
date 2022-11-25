b472240c94201064e9b2f238d58c55b58c63e08c
SZ-Coder <sz-coder@nap.software>
2022-11-25 19:20:49 +0100

    Update doc-block comments of 'napc-panic.h' (newer version of 'doc-blocks').

--- src/napc-panic/napc-panic.h
+++ src/napc-panic/napc-panic.h
@@ -12,17 +12,18 @@
 	) LIBNAPC_FN_PRINTFLIKE(4, 5);
 
 	/*!
-	 * @name NAPC_PANIC
-	 * @module Core
-	 * @brief Abort program execution.
-	 * @version 1.0.0
-	 * @description
 	 * Abort program execution and print a message.
-	 * @param _cstring_ fmt printf()-like format string.
-	 * @param n/a ... Values to incorporate into string.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * 
+	 * @module Core
+	 * @type macro:fn
+	 * @fullname LIBNAPC_PANIC
+	 * @brief Abort program execution.
+	 * @version 2.0.0
+	 * @param fmt printf()-like format string.
+	 * @variadic Values to incorporate into string.
+	 * @changelog 2.0.0 initial version
 	 * @example
-	 * NAPC_PANIC("Some error occurred: %s", "Could not open file");
+	 * LIBNAPC_PANIC("Some error occurred: %s", "Could not open file");
 	 */
 	#define LIBNAPC_PANIC(fmt, ...) \
 		libnapc_halt(__FILE__, __LINE__, __func__, fmt, ## __VA_ARGS__)
