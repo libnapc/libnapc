788fe61fa6622c06765d79cb713c9f1c9229eb97
SZ-Coder <sz-coder@nap.software>
2022-11-25 17:00:08 +0100

    Update doc-block comments of 'napc-time.h' (newer version of 'doc-blocks').

--- src/napc-time/napc-time.h
+++ src/napc-time/napc-time.h
@@ -1,32 +1,33 @@
-// needs doc-block fixing
 #if !defined(LIBNAPC_TIME_h)
 	#define LIBNAPC_TIME_h
 
 	#include <libnapc-internal.h>
 
 	/*!
-	 * @name napc_getTimeSinceBoot
-	 * @brief Get time since boot.
-	 * @version 1.0.0
-	 * @description
 	 * Return number of milliseconds that have passed since boot.
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_getTimeSinceBoot
+	 * @brief Get time since boot.
+	 * @version 2.0.0
 	 * @warning
 	 * The value returned by this function will overflow at some point in time.
-	 * @flags hw
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 */
 	libnapc_time libnapc_getTimeSinceBoot(void) NAPC_FN_WARNUNUSED_RET();
 
 	/*!
-	 * @name napc_getTimeSinceBootHR
+	 * Return number of microseconds that have passed since boot.
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_getTimeSinceBootHR
 	 * @brief Get time since boot in microseconds.
 	 * @version 2.0.0
-	 * @description
-	 * Return number of microseconds that have passed since boot.
 	 * @warning
 	 * The value returned by this function will overflow at some point in time.
-	 * @flags hw
-	 * @changelog 2.0.0 21.10.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 */
 	libnapc_time libnapc_getTimeSinceBootHR(void) NAPC_FN_WARNUNUSED_RET();
 #endif
