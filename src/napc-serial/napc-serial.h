b5f2ef6b6341a05aed0811258ccce4ba92c92aff
SZ-Coder <sz-coder@nap.software>
2022-11-25 17:03:14 +0100

    Update doc-block comments of 'napc-serial.h' (newer version of 'doc-blocks').

--- src/napc-serial/napc-serial.h
+++ src/napc-serial/napc-serial.h
@@ -1,30 +1,33 @@
-// needs doc-block fixing
 #if !defined(LIBNAPC_SERIAL_h)
 	#define LIBNAPC_SERIAL_h
 
 	#include <libnapc-internal.h>
 
 	/*!
-	 * @name napc_puts
+	 * Output a string via console or serial.
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_puts
 	 * @brief Print a string to console/serial.
-	 * @version 1.0.0
-	 * @description Output a string via console or serial.
-	 * @flags hw
+	 * @version 2.0.0
 	 * @param str The string to be printed.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * napc_puts("Hello World!");
 	 */
 	void libnapc_puts(const char *str);
 
 	/*!
-	 * @name napc_putc
+	 * Output a single character via console or serial.
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_putc
 	 * @brief Print a character to console/serial.
-	 * @version 1.0.0
-	 * @description Output a single character via console or serial.
-	 * @flags hw
+	 * @version 2.0.0
 	 * @param ch The character to be printed.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * napc_putc('H');
 	 * napc_putc('e');
@@ -36,31 +39,34 @@
 	void libnapc_putc(char ch);
 
 	/*!
-	 * @name napc_printf
-	 * @brief Print a printf()-like formatted string.
-	 * @version 1.0.0
-	 * @description
 	 * Print out information without having to call `snprintf()` first.
-	 * @flags smem
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_printf
+	 * @brief Print a printf()-like formatted string.
+	 * @version 2.0.0
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * int my_var = 13;
 	 * 
 	 * napc_printf("Value of my_var is %d", my_var);
 	 * @param fmt printf()-like format string.
-	 * @param ... Values to incorporate into string.
+	 * @variadic Values to incorporate into string.
 	 */
 	void libnapc_printf(const char *fmt, ...) LIBNAPC_FN_PRINTFLIKE(1, 2);
 
 	/*!
-	 * @name napc_mute
-	 * @brief Disable console/serial output.
-	 * @version 1.0.0
-	 * @description
 	 * Temporarily disable output of `napc_puts`, `napc_putc` and `napc_printf`.
-	 * @notes
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_mute
+	 * @brief Disable console/serial output.
+	 * @version 2.0.0
+	 * @note
 	 * This functionality is used in the unit test suite of libnapc.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * napc_printf("Hello"); // prints Hello
 	 * 
@@ -75,14 +81,16 @@
 	void libnapc_mute(void);
 
 	/*!
-	 * @name napc_unmute
-	 * @brief Enable console/serial output.
-	 * @version 1.0.0
-	 * @description
 	 * Re-enable output of `napc_puts`, `napc_putc` and `napc_printf` after a call to `napc_mute`.
-	 * @notes
+	 * 
+	 * @module Core
+	 * @type fn
+	 * @fullname napc_unmute
+	 * @brief Enable console/serial output.
+	 * @version 2.0.0
+	 * @note
 	 * This functionality is used in the unit test suite of libnapc.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * napc_printf("Hello"); // prints Hello
 	 * 
