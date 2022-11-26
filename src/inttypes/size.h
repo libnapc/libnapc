d57d49f3348c017115b6f565036a77d3060d9fce
SZ-Coder <sz-coder@nap.software>
2022-11-26 16:45:59 +0100

    Update doc-blocks of 'inttypes/size.h'.

--- src/inttypes/size.h
+++ src/inttypes/size.h
@@ -2,23 +2,25 @@
 	#define LIBNAPC_INTTYPES_SIZE_h
 
 	/*!
-	 * @name napc_size
-	 * @brief Memory size type.
-	 * @version 1.0.0
-	 * @description
 	 * Unsigned integer type for holding object sizes.
-	 * @rationale
+	 * 
+	 * @module Core
+	 * @type type:alias
+	 * @fullname libnapc_size
+	 * @brief Memory size type.
+	 * @version 2.0.0
+	 * @note
 	 * Used because Arduino lacks `size_t` in `<stddef.h>`.
 	 * 
 	 * For some reason it is defined in `<stdio.h>` but
-	 * I don't want to pollute my headers with unnecessary
+	 * we don't want to pollute the library headers with unnecessary
 	 * includes.
 	 * 
-	 * Also this way, I'm sure what printf specifiers
+	 * Also this way it is clear what printf specifiers
 	 * to use when printing `size_t` resp. `ssize_t`.
 	 * 
 	 * (`%zu` is not supported in arduino's libc)
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 */
 	typedef unsigned long libnapc_size;
 
@@ -26,16 +28,17 @@
 	#define LIBNAPC_SIZE_LITERAL(_v) _v ## ul
 
 	/*!
-	 * @name NAPC_SIZE_PRINTF
+	 * Format specifier for printing `libnapc_size` variables.
+	 * 
 	 * @module Core
-	 * @version 1.0.0
-	 * @description
-	 * Format specifier for printing `napc_size` variables.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @type macro:var
+	 * @fullname LIBNAPC_SIZE_PRINT_DEC
+	 * @version 2.0.0
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * libnapc_size var = 1;
 	 * 
-	 * napc_printf("var is = %" LIBNAPC_SIZE_PRINT_DEC, var);
+	 * libnapc_printf("var is = %" LIBNAPC_SIZE_PRINT_DEC, var);
 	 */
 	#define LIBNAPC_SIZE_PRINT_DEC "lu"
 #endif
