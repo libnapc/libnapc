cf0cbff1b1022f0aaec704557e5b316baeeb5ec8
SZ-Coder <sz-coder@nap.software>
2022-11-26 16:51:30 +0100

    Update doc-blocks of 'inttypes/16bit/libnapc_u16.h'.

--- src/inttypes/16bit/libnapc_u16.h
+++ src/inttypes/16bit/libnapc_u16.h
@@ -5,39 +5,43 @@
 	#include <inttypes.h> // PRIu16
 
 	/*!
-	 * @name napc_u16
-	 * @brief 16 bit wide unsigned integer 
-	 * @version 1.0.0
-	 * @description
 	 * Type for unsigned integers with 16 bits width.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * 
+	 * @module Core
+	 * @type type:alias
+	 * @fullname libnapc_u16
+	 * @brief 16 bit wide unsigned integer 
+	 * @version 2.0.0
+	 * @changelog 2.0.0 initial version
 	 */
 	typedef uint16_t libnapc_u16;
 
 	/*!
-	 * @name NAPC_U16_LITERAL
-	 * @module Core
-	 * @version 1.0.0
-	 * @description
 	 * Used for literal unsigned integers with 16 bits width.
-	 * @param n/a value Value
-	 * @rationale
+	 * 
+	 * @module Core
+	 * @type macro:fn
+	 * @fullname LIBNAPC_U16_LITERAL
+	 * @version 2.0.0
+	 * @param value Value
+	 * @note
 	 * This macro is only included for consistency's sake.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
-	 * napc_u16 var = NAPC_U16_LITERAL(1234);
+	 * libnapc_u16 var = LIBNAPC_U16_LITERAL(1234);
 	 */
 	#define LIBNAPC_U16_LITERAL(value) UINT16_C(value)
 
 	/*!
-	 * @name NAPC_U16_PRINTF
-	 * @module Core
-	 * @version 1.0.0
-	 * @description
 	 * Format specifier for printing `napc_u16` variables.
-	 * @rationale
+	 * 
+	 * @module Core
+	 * @type macro:var
+	 * @fullname LIBNAPC_U16_PRINT_DEC
+	 * @version 2.0.0
+	 * @note
 	 * This macro is only included for consistency's sake.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * libnapc_u16 var = 1;
 	 * 
