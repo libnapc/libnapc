c10d206f42d9b243378d9622643b247a92e9696c
SZ-Coder <sz-coder@nap.software>
2022-11-26 16:41:12 +0100

    Update doc-blocks of 'inttypes/time.h'.

--- src/inttypes/time.h
+++ src/inttypes/time.h
@@ -4,12 +4,14 @@
 	#include <inttypes/32bit/libnapc_u32.h>
 
 	/*!
-	 * @name napc_time
-	 * @brief Unsigned timestamp int type.
-	 * @version 1.0.0
-	 * @description
 	 * Unsigned integer type for holding timestamps.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * 
+	 * @module Core
+	 * @type type:alias
+	 * @fullname napc_time
+	 * @brief Unsigned timestamp int type.
+	 * @version 2.0.0
+	 * @changelog 2.0.0 initial version
 	 */
 	typedef libnapc_u32 libnapc_time;
 
@@ -17,16 +19,17 @@
 	#define LIBNAPC_TIME_LITERAL(_v) LIBNAPC_U32_LITERAL(_v)
 
 	/*!
-	 * @name NAPC_TIME_PRINTF
-	 * @module Core
-	 * @version 1.0.0
-	 * @description
-	 * Format specifier for printing `napc_time` variables.
-	 * @changelog 1.0.0 17.02.2022 initial version
-	 * @example
-	 * napc_time var = 1;
+	 * Format specifier for printing `libnapc_time` variables.
 	 * 
-	 * napc_printf("var is = %" LIBNAPC_TIME_PRINT_DEC, var);
+	 * @module Core
+	 * @type macro:var
+	 * @fullname LIBNAPC_TIME_PRINTF
+	 * @version 2.0.0
+	 * @changelog 2.0.0 initial version
+	 * @example
+	 * libnapc_time var = 1;
+	 * 
+	 * libnapc_printf("var is = %" LIBNAPC_TIME_PRINT_DEC, var);
 	 */
 	#define LIBNAPC_TIME_PRINT_DEC PRIu32
 #endif
