90e5d4015ca3dfabd884bcbe5da24780dab1dd4c
SZ-Coder <sz-coder@nap.software>
2022-11-27 13:38:44 +0100

    Rename 'NAPC_MAGIC_*' to 'LIBNAPC_MAGIC_*'.

--- src/libnapc-magic/libnapc-magic.h
+++ src/libnapc-magic/libnapc-magic.h
@@ -11,7 +11,7 @@
 
 	#define LIBNAPC_MAGIC_DESTROYED LIBNAPC_U32_LITERAL(0xFFFFFFFF)
 
-	#define NAPC_MAGIC_INIT(type, obj) (obj)->_init_magic = (NAPC_MAGIC_ ## type)
+	#define NAPC_MAGIC_INIT(type, obj) (obj)->_init_magic = (LIBNAPC_MAGIC_ ## type)
 	#define LIBNAPC_MAGIC_DESTROY(obj) (obj)->_init_magic = LIBNAPC_MAGIC_DESTROYED
 
 	// Used to silence -Waddress warnings
@@ -21,7 +21,7 @@
 	#define NAPC_MAGIC_ASSERT(type, obj)                                         \
 		do {                                                                     \
 			const libnapc_init_magic actual   = PV_LIBNAPC_MAGIC_GET_VALUE(obj); \
-			const libnapc_init_magic expected = (NAPC_MAGIC_ ## type);           \
+			const libnapc_init_magic expected = (LIBNAPC_MAGIC_ ## type);        \
 			if (actual == LIBNAPC_MAGIC_DESTROYED) {                             \
 				LIBNAPC_PANIC("Attempt to use destroyed " # type);               \
 			} else if (actual != expected) {                                     \
@@ -33,16 +33,16 @@
 	 * Is used for structs that are allocated on the stack and have an
 	 * _init() function.
 	 */
-	#define NAPC_MAGIC_napc__HRTimer                      LIBNAPC_U32_LITERAL(0x56d3a4f1)
-	#define NAPC_MAGIC_napc__Timer                        LIBNAPC_U32_LITERAL(0x2f1fa628)
-	#define NAPC_MAGIC_napc__Pool                         LIBNAPC_U32_LITERAL(0xd3d0df2a)
+	#define LIBNAPC_MAGIC_napc__HRTimer                   LIBNAPC_U32_LITERAL(0x56d3a4f1)
+	#define LIBNAPC_MAGIC_napc__Timer                     LIBNAPC_U32_LITERAL(0x2f1fa628)
+	#define LIBNAPC_MAGIC_napc__Pool                      LIBNAPC_U32_LITERAL(0xd3d0df2a)
 
-	#define NAPC_MAGIC_napc__Buffer                       LIBNAPC_U32_LITERAL(0x7beccbe2)
-	#define NAPC_MAGIC_napc__Reader                       LIBNAPC_U32_LITERAL(0xf0de7965)
+	#define LIBNAPC_MAGIC_napc__Buffer                    LIBNAPC_U32_LITERAL(0x7beccbe2)
+	#define LIBNAPC_MAGIC_napc__Reader                    LIBNAPC_U32_LITERAL(0xf0de7965)
 
-	#define NAPC_MAGIC_napc__Writer                       LIBNAPC_U32_LITERAL(0x1b81b5d1)
+	#define LIBNAPC_MAGIC_napc__Writer                    LIBNAPC_U32_LITERAL(0x1b81b5d1)
 
-	#define NAPC_MAGIC_napc__IPv4Participant              LIBNAPC_U32_LITERAL(0x97d59a3d)
+	#define LIBNAPC_MAGIC_napc__IPv4Participant           LIBNAPC_U32_LITERAL(0x97d59a3d)
 
-	#define NAPC_MAGIC_napc__RingBuffer                   LIBNAPC_U32_LITERAL(0x67fa9d2d)
+	#define LIBNAPC_MAGIC_napc__RingBuffer                LIBNAPC_U32_LITERAL(0x67fa9d2d)
 #endif
