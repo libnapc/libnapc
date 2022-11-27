e3cf222c802884a986db55e7ead32b55417d05da
SZ-Coder <sz-coder@nap.software>
2022-11-27 21:27:16 +0100

    - rename 'napc_Buffer_*' to 'libnapc_Buffer_*'. - rename 'napc__Buffer' to 'libnapc__Buffer'. - update doc-blocks of 'module/buffer/buffer.h'.

--- src/module/buffer/buffer.h
+++ src/module/buffer/buffer.h
@@ -1,4 +1,4 @@
-/*!
+/* <-- add '!' back once ported -->
  * @name Buffer:intro
  * @description
  * To reduce parameter count this module is used to pass a buffer with its size to a function:
@@ -18,55 +18,58 @@
 	#include <libnapc-magic/libnapc-magic.h>
 
 	/*!
-	 * @name napc__Buffer
 	 * @module Buffer
+	 * @type type:struct
+	 * @fullname libnapc__Buffer
 	 * @brief Representation of a memory buffer.
-	 * @version 1.0.0
+	 * @version 2.0.0
 	 * @field size Size of data.
 	 * @field data Pointer to data.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 */
-	typedef struct napc__Buffer {
+	typedef struct libnapc__Buffer {
 		LIBNAPC_MAGIC_MEMBER; // used to detect uninitialized buffers
 
 		libnapc_size size;
 		void *data;
-	} napc__Buffer;
+	} libnapc__Buffer;
 
 	/*!
-	 * @name napc_Buffer_init
+	 * @module Buffer
+	 * @type fn
+	 * @fullname libnapc_Buffer_init
+	 * @name init
 	 * @brief Initialize a buffer.
-	 * @version 1.0.0
-	 * @description
-	 * Initialize a buffer.
+	 * @version 2.0.0
 	 * @param buffer Pointer to napc__Buffer instance to be initialized.
 	 * @param data Pointer to data.
 	 * @param data_size Size of data.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * char message[512];
-	 * napc__Buffer buffer;
+	 * libnapc__Buffer buffer;
 	 * 
-	 * napc_Buffer_init(&buffer, message, sizeof(message));
+	 * libnapc_Buffer_init(&buffer, message, sizeof(message));
 	 */
-	void napc_Buffer_init(
-		napc__Buffer *buffer, void *data, libnapc_size data_size
+	void libnapc_Buffer_init(
+		libnapc__Buffer *buffer, void *data, libnapc_size data_size
 	);
 
 	/*!
-	 * @name napc_Buffer_create
+	 * @module Buffer
+	 * @type fn
+	 * @fullname libnapc_Buffer_create
+	 * @name create
 	 * @brief Create a buffer.
-	 * @version 1.0.0
-	 * @description
-	 * Create a buffer.
+	 * @version 2.0.0
 	 * @param data Pointer to data.
 	 * @param data_size Size of data.
 	 * @return Returns an initialized buffer.
-	 * @changelog 1.0.0 17.02.2022 initial version
+	 * @changelog 2.0.0 initial version
 	 * @example
 	 * char message[512];
 	 * 
-	 * napc__Buffer buffer = napc_Buffer_create(message, sizeof(message));
+	 * libnapc__Buffer buffer = libnapc_Buffer_create(message, sizeof(message));
 	 */
-	napc__Buffer napc_Buffer_create(void *data, libnapc_size data_size);
+	libnapc__Buffer libnapc_Buffer_create(void *data, libnapc_size data_size);
 #endif
--- src/module/buffer/public/create.c
+++ src/module/buffer/public/create.c
@@ -1,9 +1,9 @@
 #include <module/buffer/_private/_buffer.h>
 
-napc__Buffer napc_Buffer_create(void *data, libnapc_size data_size) {
-	napc__Buffer buffer;
+libnapc__Buffer libnapc_Buffer_create(void *data, libnapc_size data_size) {
+	libnapc__Buffer buffer;
 
-	napc_Buffer_init(&buffer, data, data_size);
+	libnapc_Buffer_init(&buffer, data, data_size);
 
 	return buffer;
 }
