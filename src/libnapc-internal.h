5e1d79a63e55c28d07f3a134de10f0c4893eb17f
SZ-Coder <sz-coder@nap.software>
2022-11-29 17:06:16 +0100

    Stop compilation if 'LIBNAPC_INCLUDE_TESTS' is set, but 'LIBNAPC_DEBUG' is not.

--- src/libnapc-internal.h
+++ src/libnapc-internal.h
@@ -6,6 +6,14 @@
 
 	#define LIBNAPC_ARRAY_ELEMENTS(array) (sizeof(array) / sizeof(* array))
 
+	/**
+	 * stop compilation if LIBNAPC_INCLUDE_TESTS is defined but LIBNAPC_DEBUG isn't.
+	 * LIBNAPC_INCLUDE_TESTS requires LIBNAPC_DEBUG to be present/set.
+	 */
+	#if defined(LIBNAPC_INCLUDE_TESTS) && !defined(LIBNAPC_DEBUG)
+		#error LIBNAPC_INCLUDE_TESTS is set, but LIBNAPC_DEBUG is not. Make sure to define both.
+	#endif
+
 	#include <stdbool.h> // bool
 	#include <stdlib.h> // free()
 	#include <stddef.h> // NULL
