1e437154764afa5006f0c86be6b2ba83ae9f91d5
SZ-Coder <sz-coder@nap.software>
2022-11-29 17:15:04 +0100

    Change 'libnapc compile' to 'libnapc compile --with-tests' to include unit tests in CI build.

--- ci/__run.sh
+++ ci/__run.sh
@@ -2,7 +2,7 @@
 
 ./bin/libnapc.php clean
 ./bin/libnapc.php preprocess
-./bin/libnapc.php compile
+./bin/libnapc.php compile --with-tests
 ./bin/libnapc.php link
 ./bin/libnapc.php run-unit-tests
 ./bin/libnapc.php run-linux-tests
