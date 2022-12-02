663fee4e96e09f4a2187679b0b7ad607d7b3a91c
SZ-Coder <sz-coder@nap.software>
2022-12-02 14:15:21 +0100

    - add 'valgrind' to '.dependencies' folder. - adjust 'libnapc run-unit-tests' to use 'valgrind' from '.dependencies' folder.

--- bin/libnapc/run-unit-tests/steps/2.executeWithValgrind.php
+++ bin/libnapc/run-unit-tests/steps/2.executeWithValgrind.php
@@ -1,8 +1,12 @@
 <?php
 
 return function($args, &$context) {
+	$getPath = require LIBNAPC_PROJECT_ROOT_DIR."/.dependencies/getPath.php";
+
+	$valgrind_bin = $getPath("valgrind");
+
 	napphp::shell_execute(
-		"valgrind", [
+		$valgrind_bin, [
 			"args" => [
 				"--leak-check=full",
 				"--show-reachable=yes",
