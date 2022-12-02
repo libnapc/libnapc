926ba234c1e0671b68d936112e18bfaa575c23c7
SZ-Coder <sz-coder@nap.software>
2022-12-02 13:22:39 +0100

    Remove need for 'NAPPHP_LOAD_PATH' by installing nap-software/napphp into a fixed location at '/opt/nap-software/napphp/'.

--- load-napphp.php
+++ load-napphp.php
@@ -1,22 +1,9 @@
 <?php
 
 (function() {
-	$NAPPHP_LOAD_PATH = getenv("NAPPHP_LOAD_PATH");
-
-	if (!strlen($NAPPHP_LOAD_PATH)) {
-		throw new Exception("Environment variable 'NAPPHP_LOAD_PATH' not set.");
-	}
-	else if (!is_file($NAPPHP_LOAD_PATH)) {
-		throw new Exception("Could not find '$NAPPHP_LOAD_PATH' (invalid NAPPHP_LOAD_PATH).");
-	}
-
-	if (defined("STDERR")) {
-		fwrite(STDERR, "NAPPHP_LOAD_PATH=$NAPPHP_LOAD_PATH\n");
-	}
-
 	// Only load napphp if not already loaded
 	if (!class_exists("napphp")) {
-		require_once $NAPPHP_LOAD_PATH;
+		require_once "/opt/nap-software/napphp/src/__loadAsClass.php";
 
 		if (defined("STDERR")) {
 			fwrite(STDERR, "Loaded nap-software/napphp version v".napphp::info_getVersion()."\n");
