29821b6fd1a8456abad621fb43ffbe3349ec1f05
SZ-Coder <sz-coder@nap.software>
2022-12-05 00:18:11 +0100

    Upgrade load-napphp.php to load nap-software/napphp starting from version v0.10.0.

--- load-napphp.php
+++ load-napphp.php
@@ -5,17 +5,12 @@
 
 	// Only load napphp if not already loaded
 	if (!class_exists("napphp")) {
-		require_once $getPathOfDependency("napphp")."/src/__loadAsClass.php";
+		require_once $getPathOfDependency("napphp")."/src/index.php";
 
 		if (defined("STDERR")) {
 			fwrite(STDERR, "Loaded nap-software/napphp version v".napphp::info_getVersion()."\n");
 			fwrite(STDERR, "Using PHP v".phpversion()."\n");
 		}
-
-		// setup tmp_dir
-		napphp::set("tmp_dir", __DIR__."/tmp.d/");
-		napphp::set("terminate_on_warning", true);
-		napphp::set("warning_print_trace", true);
 	} else {
 		if (defined("STDERR")) {
 			fwrite(STDERR, "nap-software/napphp already loaded (version=".napphp::info_getVersion().")\n");
